fa1d16e41c8e50c2e47bf9470220262a9429d0ac
szantov <152182775+szantov@users.noreply.github.com>
2024-05-14 13:40:37 +0200

    Update lekerdezes2.sql

--- lekerdezes2.sql
+++ lekerdezes2.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT TOP 1 f.vezetéknév + ' ' + f.keresztnév AS 'Fodrász neve',
-                (SUM(CAST(sz.időtartam_perc AS INT)) OVER(PARTITION BY f.fodrász_id)) / 60 AS 'Munkaidő órában'
-FROM foglalas fog JOIN fodrasz f ON fog.fodrász_id = f.fodrász_id
-					JOIN szolgaltatas sz ON fog.szolgáltatás_id = sz.szolgáltatás_id
+SELECT DISTINCT TOP 1 f.vezeteknev + ' ' + f.keresztnev AS 'Fodrász neve',
+                (SUM(CAST(sz.idotartam_perc AS INT)) OVER(PARTITION BY f.fodrasz_id)) / 60 AS 'Munkaidő órában'
+FROM foglalas fog JOIN fodrasz f ON fog.fodrasz_id = f.fodrasz_id
+					JOIN szolgaltatas sz ON fog.szolgaltatas_id = sz.szolgaltatas_id
 ORDER BY 2 DESC
