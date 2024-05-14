a92b7a2b034aca97630c3e813f5e493d3a6ad66c
szantov <152182775+szantov@users.noreply.github.com>
2024-05-14 13:50:41 +0200

    Update lekerdezes6.sql

--- lekerdezes6.sql
+++ lekerdezes6.sql
@@ -1,7 +1,7 @@
-SELECT DISTINCT u.vezetéknév + ' ' + u.keresztnév AS 'Ügyfél neve',
-    	COUNT(*) OVER(PARTITION BY u.ügyfél_id) AS 'Foglalások száma',
-    	SUM(CAST(sz.ár_Ft AS INT)) OVER(PARTITION BY u.ügyfél_id) AS 'Fizetett összeg'
-FROM ugyfel u JOIN foglalas fog ON u.ügyfél_id = fog.ügyfél_id
-				JOIN fizetes fiz ON fog.fizetés_id = fiz.fizetés_id
-                JOIN szolgaltatas sz ON fog.szolgáltatás_id = sz.szolgáltatás_id
+SELECT DISTINCT u.vezeteknev + ' ' + u.keresztnev AS 'Ügyfél neve',
+    	COUNT(*) OVER(PARTITION BY u.ugyfel_ID) AS 'Foglalások száma',
+    	SUM(CAST(sz.ar_Ft AS INT)) OVER(PARTITION BY u.ugyfel_ID) AS 'Fizetett összeg'
+FROM ugyfel u JOIN foglalas fog ON u.ugyfel_ID = fog.ugyfel_ID
+				JOIN fizetes fiz ON fog.fizetes_id = fiz.fizetes_id
+                JOIN szolgaltatas sz ON fog.szolgaltatas_ID = sz.szolgaltatas_ID
 ORDER BY 3 DESC
