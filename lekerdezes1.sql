65948f6f8ab426241663da5f9202e484c341541b
szantov <152182775+szantov@users.noreply.github.com>
2024-05-14 13:45:30 +0200

    Update lekerdezes1.sql

--- lekerdezes1.sql
+++ lekerdezes1.sql
@@ -1,6 +1,6 @@
 SELECT IIF(GROUPING_ID(f.fizetesi_eszkoz) = 1, 'Végösszeg', f.fizetesi_eszkoz) AS 'Fizetési eszköz',
         COUNT(*) AS 'Db',
         SUM(CAST(sz.ar_Ft AS INT)) AS 'Összeg (Ft)'
-FROM fizetes f JOIN foglalas fog ON fog.fizetes_id = f.fizetes_id
-                JOIN szolgaltatas sz ON sz.szolgaltatas_id = fog.szolgaltatas_id
+FROM fizetes f JOIN foglalas fog ON fog.fizetes_ID = f.fizetes_ID
+                JOIN szolgaltatas sz ON sz.szolgaltatas_ID = fog.szolgaltatas_ID
 GROUP by ROLLUP(f.fizetesi_eszkoz)
